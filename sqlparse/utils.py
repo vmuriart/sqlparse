01f1312b34bf8a8ffe0234ef3a01ece03d6a6920
Victor Uriarte <victor.m.uriarte@intel.com>
2016-06-19 18:27:45 -0700

    remove dead code from utils

--- sqlparse/utils.py
+++ sqlparse/utils.py
@@ -54,8 +54,6 @@ def split_unquoted_newlines(stmt):
 
 def remove_quotes(val):
     """Helper that removes surrounding quotes from strings."""
-    if val is None:
-        return
     if val[0] in ('"', "'") and val[0] == val[-1]:
         val = val[1:-1]
     return val
